61263be7503493f0737cc299f99bfba7755d8855
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-04-24 18:26:09 -0300

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,11 +26,11 @@ export default function Page() {
       <hr></hr>
       <a href="/api/dopmage?open=dav7.pages.dev/d-framework/icon/512.png" target="_blank">dopmage?open=dav7.pages.dev/d-framework/icon/512.png</a>
       <hr></hr>
+      <a href="/api/midia?open=Dev7" target="_blank">/midia?open=Dev7</a>
+      <hr></hr>
       <a href="/api/dyimage?username=davidsonbpe" target="_blank">dyimage?username=davidsonbpe</a>
       <hr></hr>
       <a href="/api/dynamic-image?username=davidsonbpe" target="_blank">dynamic-image?username=davidsonbpe</a>
-      <hr></hr>
-      <a href="/api/static" target="_blank">static</a>
       <hr></hr>
       
     </div>
