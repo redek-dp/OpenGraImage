67e5181043881b439d114f9fdb8bb0b15d76a8a4
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-04-24 16:58:33 -0300

    1

--- pages/api/dyimage.tsx
+++ pages/api/dyimage.tsx
@@ -24,7 +24,7 @@ export default async function handler(req: NextRequest) {
           background: '#f6f6f6',
           width: '100%',
           height: '100%',
-          paddingTop: 30,
+          paddingTop: 20,
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
