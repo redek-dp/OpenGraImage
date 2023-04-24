de5946f8240a2617663f4806b0c9e0eed92a08e9
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-04-24 12:30:12 -0300

    1

--- pages/api/iframe.tsx
+++ pages/api/iframe.tsx
@@ -10,33 +10,42 @@ export default async function handler(req: NextRequest) {
   const username = searchParams.get('username')
   if (!username) {
     return new ImageResponse(<>{'Visit with "?username=vercel"'}</>, {
-      width: 1200,
-      height: 630,
+      width: 700,
+      height: 700,
     })
   }
 
   return new ImageResponse(
     (
-      <iframe
-      src={`${username}`}
-      frameborder="0"
+      <div
         style={{
+          fontSize: 40,
           color: 'black',
           background: '#f6f6f6',
           width: '100%',
           height: '100%',
+          paddingTop: 50,
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
           display: 'flex',
         }}
       >
-        
-      </iframe>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          alt="avatar"
+          width="256"
+          src={`https://github.com/${username}.png`}
+          style={{
+            borderRadius: 128,
+          }}
+        />
+        <p>github.com/{username}</p>
+      </div>
     ),
     {
-      width: 1200,
-      height: 630,
+      width: 700,
+      height: 700,
     }
   )
 }
