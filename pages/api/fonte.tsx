35b9a3ca9431b890f06337c208910e55ab0df46d
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-04-24 15:14:30 -0300

    1

--- pages/api/fonte.tsx
+++ pages/api/fonte.tsx
@@ -33,6 +33,7 @@ export default async function handler(req: NextRequest) {
           fontFamily: 'Typewriter',
           paddingTop: '100px',
           paddingLeft: '50px',
+          paddingRight: '50px',
           lineHeight: '3.20em',
         }}
       >
