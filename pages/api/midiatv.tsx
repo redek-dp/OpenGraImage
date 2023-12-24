2ed556739ff832f65d687815a81019fc1fca8231
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-12-24 11:42:02 -0300

    55

--- pages/api/midiatv.tsx
+++ pages/api/midiatv.tsx
@@ -37,7 +37,7 @@ export default async function handler(req: NextRequest) {
             textAlign: 'center',
             width: '100px',
             height: '100px',
-            margin: 6,
+            padding: 10,
             borderRadius: 50,
             color: '#fff',
             background: 'black',
