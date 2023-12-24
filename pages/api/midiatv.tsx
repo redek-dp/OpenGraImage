358491024be6b25686aadea2235a9e1209dde315
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-12-24 10:37:01 -0300

    11

---
+++ pages/api/midiatv.tsx
@@ -0,0 +1,53 @@
+import { ImageResponse } from '@vercel/og'
+import { NextRequest } from 'next/server'
+
+export const config = {
+  runtime: 'edge',
+}
+
+export default async function handler(req: NextRequest) {
+  const { searchParams } = req.nextUrl
+  const open = searchParams.get('open')
+  if (!open) {
+    return new ImageResponse(<>{'Visit with "?open=Conteudo"'}</>, {
+      width: 512,
+      height: 512,
+    })
+  }
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 150,
+          color: 'black',
+          background: '#fff',
+          width: '100%',
+          height: '100%',
+          paddingTop: 5,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          display: 'flex',
+        }}
+      >
+        {open}
+        
+        <div
+          style={{
+            fontSize: 60,
+            marginTop: 30,
+            lineHeight: 1.8,
+          }}
+        >
+          TV
+        </div>
+
+      </div>
+    ),
+    {
+      width: 512,
+      height: 512,
+    }
+  )
+}
