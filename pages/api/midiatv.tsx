100d068d2947f873c1a28e502ada3de5963a7d06
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2023-12-24 11:48:54 -0300

    22

--- pages/api/midiatv.tsx
+++ pages/api/midiatv.tsx
@@ -19,7 +19,7 @@ export default async function handler(req: NextRequest) {
     (
       <div
         style={{
-          fontSize: '50%',
+          fontSize: '100%',
           color: 'black',
           background: '#fff',
           width: '100%',
