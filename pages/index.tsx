0872345a7573f1088b5ff28030192b149deee4c0
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2024-03-04 15:10:55 -0300

    1.6

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,8 @@ export default function Page() {
       <h1>A page with Open Graph Image.</h1>
       <a href="/api/dopp?cll=381958&open=dav7.pages.dev/d-framework/icon/512.png" target="_blank">dopp?cll=381958&open=dav7.pages.dev/d-framework/icon/512.png</a>
       <hr></hr>
+      <a href="/api/qrcode?open=00020126450014BR.GOV.BCB.PIX" target="_blank">qrcode?open=00020126450014BR.GOV.BCB.PIX</a>
+      <hr></hr>
       <a href="/api/dopx?open=dav7.pages.dev/d-framework/icon/512.png" target="_blank">dopx?open=dav7.pages.dev/d-framework/icon/512.png</a>
       <hr></hr>
       <a href="/api/fonte?open=Certamente%20que%20a%20bondade%20e%20a%20misericórdia%20me%20seguirão%20todos%20os%20dias%20da%20minha%20vida;%20e%20habitarei%20na%20casa%20do%20Senhor%20por%20longos%20dias.%20%20Salmos%2023:6." target="_blank">/fonte?open=Certamente....</a>
