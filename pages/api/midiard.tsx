b66bf09993e83279acd26dff3dcdcdc5045f9fb6
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2026-03-28 18:52:06 -0300

    1.1

--- pages/api/midiard.tsx
+++ pages/api/midiard.tsx
@@ -22,7 +22,7 @@ export default async function handler(req: NextRequest) {
         style={{
           fontSize: `${fots}`,
           color: '#fff5',
-          background: '#2c2c3945',
+          background: '#2121aa45',
           width: '100%',
           height: '100%',
           paddingTop: 5,
@@ -41,7 +41,7 @@ export default async function handler(req: NextRequest) {
             padding: 12,
             borderRadius: 50,
             color: '#0005',
-            background: '#fff5',
+            background: 'rgba(79, 179, 22, 0.33)',
             marginBottom: 4,
           }}
         >
